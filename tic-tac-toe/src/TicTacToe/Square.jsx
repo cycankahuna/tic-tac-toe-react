1d9318709b699c8077401c8d33455f118cc09697
cycankahuna <supreetvats55@gmail.com>
2023-09-19 15:53:14 +0530

    adding board and square  components

---
+++ tic-tac-toe/src/TicTacToe/Square.jsx
@@ -0,0 +1,18 @@
+const Square = () => {
+    return (
+        <div
+            style={{
+                border: "1px solid",
+                height: "100px",
+                width: "100%",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center"
+            }}
+            className="square">
+            <h5>X</h5>
+        </div>
+    )
+
+}
+export default Square
